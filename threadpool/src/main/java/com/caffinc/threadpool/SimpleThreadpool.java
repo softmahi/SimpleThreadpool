4469acd2c3bcb7202b832daf646a34f24f0dc340
Sriram Keerthi Madhava Kunjathur <Sriram Keerthi Madhava Kunjathur>
2016-03-17 10:10:03 +0530

    Fattening the documentation

--- threadpool/src/main/java/com/caffinc/threadpool/SimpleThreadpool.java
+++ threadpool/src/main/java/com/caffinc/threadpool/SimpleThreadpool.java
@@ -2,28 +2,38 @@ package com.caffinc.threadpool;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
- * Simple Thread Pool
+ * Simple Thread Pool which provides a simple fixed threadpool
  *
  * @author Sriram
  */
 public class SimpleThreadpool {
+    // Count of threadpools created
+    private static AtomicInteger poolCount = new AtomicInteger(0);
+    // Queue of runnables
     private ConcurrentLinkedQueue<Runnable> runnables;
+    // Flag to control the SimpleThreadpoolThread objects
     private AtomicBoolean execute;
+    // Holds the "pool" of threads
     private List<SimpleThreadpoolThread> threads;
 
-
+    /**
+     * Thrown when there's a RuntimeException or InterruptedException when executing a runnable from the queue
+     */
     private class ExecutionException extends RuntimeException {
         public ExecutionException(Throwable cause) {
             super(cause);
         }
     }
 
+    /**
+     * Inner Thread class which represents the threads in the pool. It acts as a wrapper for all runnables in the queue.
+     */
     private class SimpleThreadpoolThread extends Thread {
         private AtomicBoolean execute;
         private ConcurrentLinkedQueue<Runnable> runnables;
@@ -37,38 +47,64 @@ public class SimpleThreadpool {
         @Override
         public void run() {
             try {
+                // Continue to execute when the execute flag is true, or when there are runnables in the queue
                 while (execute.get() || !runnables.isEmpty()) {
                     Runnable runnable;
+                    // Poll a runnable from the queue and execute it
                     while ((runnable = runnables.poll()) != null) {
                         runnable.run();
                     }
+                    // Sleep in case there wasn't any runnable in the queue. This helps to avoid hogging the CPU.
                     Thread.sleep(1);
                 }
-            } catch (Exception e) {
+            } catch (RuntimeException | InterruptedException e) {
                 throw new ExecutionException(e);
             }
         }
     }
 
+    /**
+     * Private constructor to control the creation of threadpools. Increments the poolcount whenever a new pool is created.
+     *
+     * @param threadCount Number of SimpleThreadpoolThreads to add to the pool
+     */
     private SimpleThreadpool(int threadCount) {
+        // Increment pool count
+        poolCount.incrementAndGet();
         this.runnables = new ConcurrentLinkedQueue<>();
         this.execute = new AtomicBoolean(true);
         this.threads = new ArrayList<>();
         for (int threadIndex = 0; threadIndex < threadCount; threadIndex++) {
-            SimpleThreadpoolThread thread = new SimpleThreadpoolThread("SimpleThreadpoolThread" + threadIndex, this.execute, this.runnables);
+            SimpleThreadpoolThread thread = new SimpleThreadpoolThread("SimpleThreadpool" + poolCount.get() + "Thread" + threadIndex, this.execute, this.runnables);
             thread.start();
             this.threads.add(thread);
         }
     }
 
+    /**
+     * Gets a new threadpool instance with number of threads equal to the number of processors (or CPU threads) available
+     *
+     * @return new SimpleThreadpool
+     */
     public static SimpleThreadpool getInstance() {
         return getInstance(Runtime.getRuntime().availableProcessors());
     }
 
+    /**
+     * Gets a new threadpool instance with the number of threads specified
+     *
+     * @param threadCount Threads to add to the pool
+     * @return new SimpleThreadpool
+     */
     public static SimpleThreadpool getInstance(int threadCount) {
         return new SimpleThreadpool(threadCount);
     }
 
+    /**
+     * Adds a runnable to the queue for processing
+     *
+     * @param runnable Runnable to be added to the pool
+     */
     public void execute(Runnable runnable) {
         if (this.execute.get()) {
             runnables.add(runnable);
@@ -77,6 +113,13 @@ public class SimpleThreadpool {
         }
     }
 
+    /**
+     * Awaits up to <b>timeout</b> ms the termination of the threads in the threadpool
+     *
+     * @param timeout Timeout in milliseconds
+     * @throws TimeoutException      Thrown if the termination takes longer than the timeout
+     * @throws IllegalStateException Thrown if the stop() or terminate() methods haven't been called before awaiting
+     */
     public void awaitTermination(long timeout) throws TimeoutException {
         if (this.execute.get()) {
             throw new IllegalStateException("Threadpool not terminated before awaiting termination");
@@ -97,6 +140,12 @@ public class SimpleThreadpool {
         throw new TimeoutException("Unable to terminate threadpool within the specified timeout (" + timeout + "ms)");
     }
 
+
+    /**
+     * Awaits the termination of the threads in the threadpool indefinitely
+     *
+     * @throws IllegalStateException Thrown if the stop() or terminate() methods haven't been called before awaiting
+     */
     public void awaitTermination() throws TimeoutException {
         if (this.execute.get()) {
             throw new IllegalStateException("Threadpool not terminated before awaiting termination");
@@ -115,11 +164,17 @@ public class SimpleThreadpool {
         }
     }
 
+    /**
+     * Clears the queue of runnables and stops the threadpool. Any runnables currently executing will continue to execute.
+     */
     public void terminate() {
         runnables.clear();
         stop();
     }
 
+    /**
+     * Stops addition of new runnables to the threadpool and terminates the pool once all runnables in the queue are executed.
+     */
     public void stop() {
         execute.set(false);
     }
